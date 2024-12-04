0be652fb61038ec1e43bb7f5079a9d39b210e6f5
Rocio Ferreiro <rocio.ferreirorico@ing.austral.edu.ar>
2024-12-04 19:45:23 -0300

    Refactor WindowProgressBar component to remove material dependency

--- src/WindowProgressBar.tsx
+++ src/WindowProgressBar.tsx
@@ -1,6 +1,4 @@
-import {Box, Grid, Tooltip } from '@mui/material';
-import * as React from 'react';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import React from "react";
 
 type Props = {
     value: number,
@@ -8,47 +6,78 @@ type Props = {
     sectors?: {min:number, max:number}[],
     colors?: string[],
     height?: number,
+    sectorWidth?: number,
     labels?: boolean,
     tooltip?: boolean
 }
 
-export function WindowProgressBar(props: Props) {
+const WindowProgressBar = (props: Props) => {
 
     const savedColors = ['#f1a636', '#f1d836','#c8f136', '#f1d836'];
 
     const max_ = props.max? props.max: 10;
     const height_ = props.height? props.height: 20;
-    const sectors_ = props.sectors? props.sectors : [{min:0, max:2}, {min:2, max:5}, {min:5, max:8}, {min:8, max:10}]
-    const colors_ = props.colors? props.colors : sectors_.map((s, i) => savedColors[i%4] )
+    const wigth_ = props.sectorWidth? props.sectorWidth: 20;
+    const sectors_ = props.sectors? props.sectors : [{min:1, max:3}, {min:3, max:6}, {min:6, max:9}, {min:9, max:11}]
+    const colors_ = props.colors? props.colors : sectors_.map((_, i) => savedColors[i%4] )
     const labels_ = props.labels ? props.labels : false;
     const tooltip_ = props.tooltip ? props.tooltip : false;
 
-    if(sectors_[sectors_.length-1].max !== max_) throw new Error("Max should be the same as the last sections max.")
+    if(sectors_[sectors_.length-1].max !== max_+1) throw new Error("Max should be the same as the last sections max.")
     if(colors_.length !== sectors_.length) throw new Error("There should be the same amount of colors than sections.")
 
 
-    let align = 'center';
-    for (let i = 0; i < sectors_.length; i++) {
-        if(props.value === sectors_[i].min+1) align = 'left';
-        else if(props.value === sectors_[i].max) align = 'right';
+    let align: 'center' | 'left' | 'right' = 'center';
+    for (const sector of sectors_) {
+        if (props.value === sector.min + 1) align = 'left';
+        else if (props.value === sector.max) align = 'right';
     }
 
-    const bar = <Box style={{display: 'flex'}}>
+    const inSection = (index: number): number => {
+        for (let i = 0; i < sectors_.length; i++) {
+            if (index >= sectors_[i].min && index < sectors_[i].max) return i;
+        }
+        return -1
+    }
+
+    const bar = <div style={{display: 'flex'}}>
         {labels_ && <p>{sectors_[0].min}</p>}
-        <Grid container style={{ flexWrap: 'nowrap'}}>
-            {sectors_.map((s,index) =>
-                <Grid item key={index} xs={s.max - s.min} md={s.max - s.min} lg={s.max - s.min} style={{backgroundColor: colors_[index], height: height_, textAlign: align === 'center'? 'center': align === 'left'? 'left' : 'right'}}>
-                    {props.value>s.min && props.value<=s.max && <ArrowDropUpIcon style={{fontSize: 35}}/>}
-                </Grid>)}
-        </Grid>
+        <div style={{display: 'grid', gridTemplateColumns: `repeat(${max_}, 1fr)`, gap: 0, width: max_*wigth_}}>
+            {Array.from({length: max_}, (_, i) => i+1).map((index) =>
+                <div key={`section-${index}`} style={{backgroundColor: colors_[inSection(index)], height: height_, textAlign: align, gridColumn: index, gridRow: 1, minWidth: wigth_}}>
+                    {props.value===index && <img src="ArrowMarker.png" alt='marker' style={{height: height_, zIndex: 10, position: 'relative'}}/>}
+                </div>)
+            }
+        </div>
         {labels_ && <p>{max_}</p>}
-    </Box>;
+    </div>;
 
     if(tooltip_){
-        return <Tooltip title={props.value}>
-            {bar}
-        </Tooltip>
+        return (
+            <div style={{position: 'relative', display: 'inline-block'}}>
+                {bar}
+                <span style={{
+                    visibility: 'hidden',
+                    backgroundColor: 'black',
+                    color: '#fff',
+                    textAlign: 'center',
+                    borderRadius: '6px',
+                    padding: '5px 0',
+                    position: 'absolute',
+                    zIndex: 1,
+                    bottom: '125%',
+                    left: '50%',
+                    marginLeft: '-60px',
+                    width: '120px',
+                    opacity: 0.8
+                }}>
+                    {`Value: ${props.value}`}
+                </span>
+            </div>
+        );
     }
 
     return bar
 }
+
+export default WindowProgressBar;
